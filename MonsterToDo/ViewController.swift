836584e17d44adc17b1fa42820b37ddd2b74d430
Nagiko Tatsui <n.t.woody.0914@gmail.com>
2021-03-09 20:46:56 +0900

    課題クエスト　セル削除

--- MonsterToDo/ViewController.swift
+++ MonsterToDo/ViewController.swift
@@ -95,28 +95,31 @@ class ViewController: UIViewController, UITableViewDataSource {
         table.rowHeight = 70
     }
     
-    //MARK: -スワイプしたセルの削除
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == UITableViewCell.EditingStyle.delete{
-            todoQuest.remove(at: indexPath.row)
-            todoDate.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
-            //userDefaultsへ書込
-            UserDefaults.standard.set(todoQuest, forKey: "quest")
-            UserDefaults.standard.set(todoDate, forKey: "date")
-            UserDefaults.standard.set(todoMonster, forKey: "monster")
-        }
-        
-        print("e")
-        print(todoQuest)
-        print(todoDate)
-    }
+//MARK: -スワイプしたセルの削除
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == UITableViewCell.EditingStyle.delete{
+//            todoQuest.remove(at: indexPath.row)
+//            todoDate.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+//            //userDefaultsへ書込
+//            UserDefaults.standard.set(todoQuest, forKey: "quest")
+//            UserDefaults.standard.set(todoDate, forKey: "date")
+//            UserDefaults.standard.set(todoMonster, forKey: "monster")
+//        }
+//
+//        print("e")
+//        print(todoQuest)
+//        print(todoDate)
+//    }
     
     //MARK: - タスク完了アクション
     //タスクの完了を設定
     @IBAction func fight(_ sender: UIButton){
         
-        print("pushed Button")
+        //Int型からIndexPath型にキャスト
+        let indexPath = IndexPath(row: sender.tag, section: 0)
+        //indexPath型でセルを指定可能
+        //let cell = self.table.cellForRow(at: indexPath)
         
         //アラートコントローラ：タイトル、メッセージ、アラートスタイルを設定
         let alertController = UIAlertController(title: "モンスターと戦う", message: "タスクを完了しましたか？", preferredStyle: .alert)
@@ -124,13 +127,28 @@ class ViewController: UIViewController, UITableViewDataSource {
         //アクション：ボタンの文字、ボタンスタイル、ボタンを押した時の処理を設定
         let cancelAction = UIAlertAction(title: "苦戦中...", style: .cancel, handler: nil)
         
-        let okAction = UIAlertAction(title: "任務完了", style: .default, handler: {action in print("wow")})
+        
+        
+        //タスクが完了したらTableViewから該当タスクを消去、図鑑に登録
+        let okAction = UIAlertAction(title: "任務完了", style: .default, handler: {(action: UIAlertAction!) in
+            todoQuest.remove(at: indexPath.row)
+            todoDate.remove(at: indexPath.row)
+            self.table.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+            //userDefaultsへ書込
+            UserDefaults.standard.set(todoQuest, forKey: "quest")
+            UserDefaults.standard.set(todoDate, forKey: "date")
+            
+            print(todoQuest)
+            print(todoDate)
+        })
         
         alertController.addAction(cancelAction)
         alertController.addAction(okAction)
         
         self.present(alertController, animated: true, completion: nil)
+        print("pushed Button")
     }
+    
 
 }
 
